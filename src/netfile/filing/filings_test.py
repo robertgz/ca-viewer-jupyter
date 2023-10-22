c84b11152aaf1ce762baa72eaa0ac4ff582c0a03
Robert <robertgz@users.noreply.github.com>
2023-10-22 13:13:58 -0700

    Updated and moved file for tests in netfile.filing

--- src/netfile/filing/filings_test.py
+++ src/netfile/filing/filings_test.py
@@ -1,4 +1,4 @@
-import filing
+from ...netfile.filing import filings
 
 def test_get_unique_filings_1():
     test_filings = [
@@ -9,11 +9,12 @@ def test_get_unique_filings_1():
         { 'id': 'test_4', 'filerName': 'Filer name 4' },
         { 'id': 'test_4', 'filerName': 'Filer name 4' },
     ]
-    response_filings = filing.get_unique_list(test_filings)
+    response_filings = filings.get_unique_list(test_filings)
     assert len(response_filings) == 4
 
 def test_get_filter_filings_1():
-    filing._filing_list = []
+    filing = filings.Filings()
+    filing.filing_list = []
     test_filings = [
         { 'id': 'test_1', 'filerName': 'Filer name 1' },
         { 'id': 'test_2', 'filerName': 'Filer name 2' },
@@ -22,7 +23,8 @@ def test_get_filter_filings_1():
     assert len(filtered) == len(test_filings)
 
 def test_get_filter_filings_2():
-    filing._filing_list = [
+    filing = filings.Filings()
+    filing.filing_list = [
         { 'id': 'test_1', 'filerName': 'Filer name 1' },
         { 'id': 'test_2', 'filerName': 'Filer name 2' },
     ]
@@ -36,16 +38,19 @@ def test_get_filter_filings_2():
     assert filtered[0]['id'] == 'test_3'
 
 def test_get_year_from_title_1():
+    filing = filings.Filings()
     title = 'FPPC Form 460 (10/18/2020 - 12/31/2020)'
     result = filing.get_year_from_title(title)
     assert result == '2020'
 
 def test_get_year_from_title_2():
+    filing = filings.Filings()
     title = 'FPPC Form 460 (10/18/2020 - )'
     result = filing.get_year_from_title(title)
     assert result == '2020'
 
 def test_get_year_from_title_3():
+    filing = filings.Filings()
     title = 'FPPC Form 460 ( - 12/31/2020)'
     result = filing.get_year_from_title(title)
     assert result == ''
@@ -53,7 +58,8 @@ def test_get_year_from_title_3():
 # def test__add_fields_1():
 
 def test_add_filings_1():
-    filing._filing_list = []
+    filing = filings.Filings()
+    filing.filing_list = []
     agency_shortcut = 'test_agency'
 
     test_filings1 = [
@@ -76,5 +82,5 @@ def test_add_filings_1():
     filing.add_filings(agency_shortcut, test_filings1)
     filing.add_filings(agency_shortcut, test_filings2)
     filing.add_filings(agency_shortcut, test_filings3)
-    assert len(filing._filing_list) == 3
-    assert filing._filing_list[0]['agencyShortcut'] == agency_shortcut
+    assert len(filing.filing_list) == 3
+    assert filing.filing_list[0]['agencyShortcut'] == agency_shortcut
