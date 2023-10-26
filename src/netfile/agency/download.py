5a5bc2363ffb9a9d17d387a6892789f3b092fbe9
Robert <robertgz@users.noreply.github.com>
2023-10-26 14:11:18 -0700

    Added return type to download_agencies

--- src/netfile/agency/download.py
+++ src/netfile/agency/download.py
@@ -1,9 +1,21 @@
 import requests
+from typing import TypedDict, Optional
+
+from ..response_status_type import NetFileResponseStatus
+
+class NetFileDownloadedAgency(TypedDict):
+    id: int
+    shortcut: str
+    name: str
+
+class NetFileDownloadedAgencyResponse(TypedDict):
+    agencies: NetFileDownloadedAgency
+    responseStatus: Optional[NetFileResponseStatus]
 
 agencies_url = 'https://www.netfile.com/Connect2/api/public/campaign/agencies.json'
 
 # Request the agency list from NetFile
-def download_agencies():
+def download_agencies() -> NetFileDownloadedAgencyResponse:
     global agencies_url
     response = requests.get(agencies_url)
 
