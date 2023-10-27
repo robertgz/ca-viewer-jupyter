8e3127c0fa74b81bc306acd1b8495e767f080707
Robert <robertgz@users.noreply.github.com>
2023-10-26 19:02:06 -0700

    Added class Agencies

---
+++ src/storage/agency/agencies.py
@@ -0,0 +1,27 @@
+
+from typing import List
+
+from .base_agency import BaseAgency
+from .netfile_agency import NetFileAgency
+from .efile_agency import EFileAgency
+
+class Agencies:
+    def __init__(self):
+        self.common_agencies: List[BaseAgency] = []
+        self.net_file_agencies_loaded = False
+        self.e_file_agencies_loaded = False
+    
+    def add_agencies(self):
+        if not self.net_file_agencies_loaded:
+            net_file_agencies = NetFileAgency.get_agencies()
+            self.common_agencies.extend(net_file_agencies)
+            self.net_file_agencies_loaded = True
+        
+        if not self.e_file_agencies_loaded:
+            e_file_agencies = EFileAgency.get_agencies()
+            self.common_agencies.extend(e_file_agencies)
+            self.e_file_agencies_loaded = True
+
+    def getAgencies(self):
+        pass
+    
