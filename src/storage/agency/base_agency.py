8744fffba264c801e2f6919187933b40b1b9b17d
Robert <robertgz@users.noreply.github.com>
2023-10-26 19:00:22 -0700

    Added class BaseAgency

---
+++ src/storage/agency/base_agency.py
@@ -0,0 +1,20 @@
+
+from dataclasses import dataclass
+from typing import List
+
+@dataclass(kw_only=True)
+class BaseAgency:
+    def get_name() -> str:
+        pass
+
+    def get_agency_shortcut() -> str:
+        pass
+
+    # def get_years() -> List(str):
+    #     pass
+
+    @staticmethod
+    def get_agencies():
+        pass
+
+    
