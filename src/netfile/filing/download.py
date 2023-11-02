29f59f7df4e06e3e25b01e96f0ff237b72ec467e
Robert <robertgz@users.noreply.github.com>
2023-11-02 09:35:25 -0700

    Added int types to form parameters

--- src/netfile/filing/download.py
+++ src/netfile/filing/download.py
@@ -24,7 +24,7 @@ class NetFileDownloadedFilingResponse(TypedDict):
     totalMatchingPages: int
 
 # function to request a page of filings from NetFile
-def get_filings(agency_shortcut: str, page=0, form=None) -> NetFileDownloadedFilingResponse:
+def get_filings(agency_shortcut: str, page=0, form: int = None) -> NetFileDownloadedFilingResponse:
     filing_url = 'https://www.netfile.com/Connect2/api/public/list/filing'
     payload = {
         "AID": agency_shortcut,
@@ -43,7 +43,7 @@ def get_filings(agency_shortcut: str, page=0, form=None) -> NetFileDownloadedFil
     return response.json()
 
 # generator function to get each page of filings for an agency
-def download_all_filings_gen_func(agency_shortcut: str, form=None):
+def download_all_filings_gen_func(agency_shortcut: str, form: int = None):
     pageNumber = 0
     getNextPage = True
 
@@ -57,7 +57,7 @@ def download_all_filings_gen_func(agency_shortcut: str, form=None):
         pageNumber += 1
         getNextPage = json['totalMatchingPages'] > pageNumber
 
-def get_all_agency_filings(agency_shortcut: str, form=None) -> list[NetFileDownloadedFiling]:
+def get_all_agency_filings(agency_shortcut: str, form: int = None) -> list[NetFileDownloadedFiling]:
     filing_list = []
     try:
         for filings in download_all_filings_gen_func(agency_shortcut, form):
